f365f695fcbf45f487d2486292abfdd644685708
GyuhwanKim <gyuhwan.a.kim@gmail.com>
2022-02-05 18:32:18 +0900

    Add apprunner resources

---
+++ ap-northeast-2/apprunner/alpha/backend/role.tf
@@ -0,0 +1,22 @@
+resource "aws_iam_policy" "apprunner" {
+  name = "alpha-kr-apprunner-base"
+  policy = file("policies/apprunner.json")
+}
+
+resource "aws_iam_role" "backend" {
+  name = "alpha-kr-apprunner-backend"
+
+  managed_policy_arns = [aws_iam_policy.apprunner.arn]
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
---
+++ ap-northeast-2/apprunner/alpha/backend/terraform.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_version = "1.1.5"
+
+  backend "remote" {
+    hostname     = "app.terraform.io"
+    organization = "tastie"
+
+    workspaces {
+      name = "aws-apne2-alpha-apprunner"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.70.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-northeast-1"
+}
